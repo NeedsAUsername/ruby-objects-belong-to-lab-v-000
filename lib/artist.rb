d59e84aff5f25211bd75605fd33007ecc8de7a05
Kevin Chi <kevinchi118@gmail.com>
2018-06-20 20:46:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,15 @@
 class Artist
     attr_accessor :name
 
+    def initialize(name)
+        @name = name
+    end 
 end
 
 class Song
     attr_accessor :title, :artist
 
-
+    def initialize(title)
+        @title = title
+    end
 end
